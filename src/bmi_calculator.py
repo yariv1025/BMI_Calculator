b352a8310da52962c4248de3bd06cca165070f10
Miyu Levin <4evermiyu@gmail.com>
2020-01-08 23:36:51 +0200

    Function implmeneted

--- src/bmi_calculator.py
+++ src/bmi_calculator.py
@@ -2,5 +2,26 @@ def bmi_calc(body_mass, body_height):
     pass
 
 
-def __bmi_number_calc(body_mass, body_height):
-    pass
+def category_by_bmi(bmi_value):
+    if bmi_value <= 0:
+        return "Error"
+    if bmi_value < 15:
+        return "Very severely underweight"
+    if 16 > bmi_value >= 15:
+        return "Severely underweight"
+    if 18.5 > bmi_value >= 16:
+        return "Underweight"
+    if 25 > bmi_value >= 18.5:
+        return "Normal"
+    if 35 > bmi_value >= 30:
+        return "Moderately obese"
+    if 40 > bmi_value >= 35:
+        return "Severely obese"
+    if 45 > bmi_value >= 40:
+        return "Very severely obese"
+    if 50 > bmi_value >= 45:
+        return "Morbidly obese"
+    if 60 > bmi_value >= 50:
+        return "Super obese"
+    if bmi_value >= 60:
+        return "Hyper obese"
---
+++ test/category_by_bmi.py
@@ -0,0 +1,75 @@
+import unittest
+from src.bmi_calculator import category_by_bmi
+
+
+class TestCategoryByBMI(unittest.TestCase):
+    def test_Negative_BMI(self):
+        stub = -1
+
+        expected = "Error"
+
+        result = category_by_bmi(stub)
+
+        self.assertEqual(result, expected)
+
+    def test_NotNormal_BMI(self):
+        stub1 = 60
+        expctednot = "Normal"
+        result1 = category_by_bmi(stub1)
+
+        self.assertNotEqual(result1, expctednot)
+
+    def test_Obese_BMI(self):
+        stub1 = 31.5
+        stub2 = 37
+        stub3 = 41
+        stub4 = 49.5
+        stub5 = 55
+        stub6 = 70
+
+        expected1 = "Moderately obese"
+        expected2 = "Severely obese"
+        expected3 = "Very severely obese"
+        expected4 = "Morbidly obese"
+        expected5 = "Super obese"
+        expected6 = "Hyper obese"
+
+        result1 = category_by_bmi(stub1)
+        result2 = category_by_bmi(stub2)
+        result3 = category_by_bmi(stub3)
+        result4 = category_by_bmi(stub4)
+        result5 = category_by_bmi(stub5)
+        result6 = category_by_bmi(stub6)
+
+        self.assertEqual(result1, expected1)
+        self.assertEqual(result2, expected2)
+        self.assertEqual(result3, expected3)
+        self.assertEqual(result4, expected4)
+        self.assertEqual(result5, expected5)
+        self.assertEqual(result6, expected6)
+
+    def test_Underweight_BMI(self):
+        # stub
+        stub = 18
+
+        # assume
+        expected = "Underweight"
+
+        # action
+        result = category_by_bmi(stub)
+
+        # expect / assert
+        self.assertEqual(result, expected)
+
+    def test_Normal_BMI(self):
+        stub1 = 21.5
+        stub2 = 25
+
+        expected1 = "Normal"
+
+        result =  category_by_bmi(stub1)
+
+        self.assertEqual(result, expected1)
+
+if __name__ == '__main__':
+    unittest.main()

e59d27cccd41fb04e955844be54f11fb02f254a7
Yariv Garala <yariv1052@gmail.com>
2020-01-11 22:03:36 +0200

    Fix printing - line 10

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from src.bubble_sort import *
 
 def main():
     # BMI Calc
-    body_mass = input("Please Enter body mass:")
+    body_mass = input("Please Enter body mass in kg:")
     body_height = input("Please Enter body height in meters:")
 
-    print("your BMI category is : " + category_by_bmi( bmi_calc(body_mass, body_height)))
+    print("your BMI category is : " + str(category_by_bmi(bmi_calc(body_mass, body_height))))
 
     # Bubble sort
     array = []

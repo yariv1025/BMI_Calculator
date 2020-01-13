1c42a8616b89d2b3c624e803494710cb646e6b60
Yariv Garala <yariv1052@gmail.com>
2020-01-13 17:15:33 +0200

    Update

--- src/bmi_calculator.py
+++ src/bmi_calculator.py
@@ -3,7 +3,7 @@ from src.is_number import IsNumber
 
 def bmi_calc(body_mass, body_height):
     if (IsNumber(body_mass) and IsNumber(body_height)):
-        body_mass=float(body_mass)
+        body_mass = float(body_mass)
         body_height = float(body_height)
         if (body_mass > 0 and body_mass < 150 and body_mass != None):
             if (body_height > 0 and body_height < 2.5 and body_height != None):
